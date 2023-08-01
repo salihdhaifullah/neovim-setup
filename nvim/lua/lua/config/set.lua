5a7921f9a582b224c011760447502527448d6460
salehWeb <salehwebdev2004@gmail.com>
2023-08-01 23:58:13 +0200

    setup

---
+++ nvim/lua/lua/config/set.lua
@@ -0,0 +1,36 @@
+vim.g.mapleader = " "
+
+vim.opt.guicursor = ""
+
+vim.opt.nu = true
+
+vim.opt.clipboard = "unnamedplus"
+vim.opt.mouse = "a"
+vim.cmd("filetype plugin indent on")
+vim.cmd([[colorscheme gruvbox]])
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = "80"
