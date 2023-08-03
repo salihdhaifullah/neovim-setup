317058a98e1708507999ce054229dde049b626ca
salehWeb <salehwebdev2004@gmail.com>
2023-08-04 00:08:46 +0200

    set up

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -7,8 +7,26 @@ vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
+
 vim.keymap.set("n", "N", "Nzzzv")
 
+function Copy_last_message()
+    local messages = vim.api.nvim_exec("messages", true)
+    local lines = vim.split(messages, "\n")
+    local last_message = ""
+
+    for i = #lines, 1, -1 do
+        if lines[i] ~= "" then
+            last_message = lines[i]
+            break
+        end
+    end
+
+    vim.fn.setreg('*', last_message)
+    vim.fn.setreg('+', last_message)
+end
+
+vim.keymap.set("n", "<leader>e", "<cmd>lua Copy_last_message()<CR>", { noremap = true })
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
