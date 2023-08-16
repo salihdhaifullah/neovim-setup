b3ad7df03d0f69b53087b83791c6d6a210ba7ea8
salehWeb <salehwebdev2004@gmail.com>
2023-08-16 23:23:36 +0200

    clean up and fix harpoon plus treesitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,14 +1,14 @@
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "c_sharp", "cpp", "css", "dockerfile",
- 	 "graphql", "go", "gosum", "html", "http", "java", "json",
- 	 "python", "prisma", "markdown", "javascript", "typescript", "c",
- 	 "lua", "sql", "tsx", "vim", "vimdoc", "query" },
+require 'nvim-treesitter.configs'.setup {
+    ensure_installed = { "c_sharp", "cpp", "css", "dockerfile",
+        "graphql", "go", "gosum", "html", "http", "java", "json",
+        "python", "prisma", "markdown", "javascript", "typescript", "c",
+        "lua", "sql", "tsx", "vim", "vimdoc", "query" },
 
-  sync_install = false,
-  auto_install = true,
+    sync_install = false,
+    auto_install = true,
 
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    },
 }
--- lua/config/packer.lua
+++ lua/config/packer.lua
@@ -1,38 +1,70 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
+    -- Packer can manage itself
     use 'wbthomason/packer.nvim'
+
+    -- Telescope for fuzzy finding and more
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.2',
-        requires = { { 'nvim-lua/plenary.nvim' } }
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.2',
+        requires = { 'nvim-lua/plenary.nvim' }
     }
-    use({
+
+    -- Rose-pine colorscheme
+    use {
         'rose-pine/neovim',
         as = 'rose-pine',
         config = function()
             vim.cmd('colorscheme rose-pine')
         end
-    })
-    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-    use "nvim-lua/plenary.nvim"
-    use "jose-elias-alvarez/null-ls.nvim"
-    use 'ThePrimeagen/harpoon'
-    use 'mbbill/undotree'
-    use { 'mg979/vim-visual-multi', branch = 'master' }
+    }
+
+    -- Treesitter for syntax highlighting and more
     use {
-        "folke/lsp-trouble.nvim",
-        requires = "kyazdani42/nvim-web-devicons",
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+    }
+
+    -- Plenary for utility functions
+    use 'nvim-lua/plenary.nvim'
+
+    -- Null-ls for using Neovim as a language server
+    use 'jose-elias-alvarez/null-ls.nvim'
+
+    -- Harpoon for quick navigation and terminal management
+    use 'ThePrimeagen/harpoon'
+
+    -- Undotree for visualizing the undo history
+    use 'mbbill/undotree'
+
+    -- Visual-multi for multiple cursors
+    use {
+        'mg979/vim-visual-multi',
+        branch = 'master'
+    }
+
+    -- Lsp-trouble for showing LSP diagnostics in a list
+    use {
+        'folke/lsp-trouble.nvim',
+        requires = 'kyazdani42/nvim-web-devicons',
         config = function()
-            require("trouble").setup {}
+            require('trouble').setup {}
         end
     }
+
+    -- Comment for easy commenting of code
     use {
         'numToStr/Comment.nvim',
         config = function()
             require('Comment').setup()
         end
     }
+
+    -- Fugitive for Git integration
     use 'tpope/vim-fugitive'
+
+    -- Lsp-zero for LSP configuration and completion
     use {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v1.x',
