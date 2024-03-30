5e0a802ac19b676278862d7f6b7d1826796df956
salihdhaifullah <salihdhaifullah@gmail.com>
2024-03-30 08:15:54 +0200

    add nvim-tree

--- lua/config/packer.lua
+++ lua/config/packer.lua
@@ -13,6 +13,13 @@ return require('packer').startup(function(use)
         requires = { 'nvim-lua/plenary.nvim' }
     }
 
+    use {
+        'nvim-tree/nvim-tree.lua',
+        requires = {
+            'nvim-tree/nvim-web-devicons', -- optional
+        },
+    }
+
     -- Rose-pine colorscheme
     use {
         'rose-pine/neovim',
